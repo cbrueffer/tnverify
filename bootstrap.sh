73a35f6e9c9c26cdf6ff1a3cb36019b996fc0167
Christian Brueffer <christian@brueffer.de>
2014-09-25 14:02:52 +0200

    Add more packages to Vagrant bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,8 @@
 apt-get update
 apt-get install -y python-scipy
 apt-get install -y python-matplotlib
+apt-get install -y python-pip
 apt-get install -y git
+apt-get install -y vim
+apt-get install -y samtools
+pip install pysam
